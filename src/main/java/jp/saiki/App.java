82a100a6b8bd60dae1e05309812f26655980af6a
saiki <akio.saiki@gmail.com>
2020-04-12 13:32:21 +0900

    initial commit.

---
+++ src/main/java/jp/saiki/App.java
@@ -0,0 +1,109 @@
+package jp.saiki;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.gax.rpc.ApiException;
+import com.google.common.collect.ImmutableList;
+import com.google.photos.library.v1.PhotosLibraryClient;
+import com.google.photos.library.v1.proto.BatchCreateMediaItemsResponse;
+import com.google.photos.library.v1.proto.NewMediaItem;
+import com.google.photos.library.v1.proto.NewMediaItemResult;
+import com.google.photos.library.v1.upload.UploadMediaItemRequest;
+import com.google.photos.library.v1.upload.UploadMediaItemResponse;
+import com.google.photos.library.v1.upload.UploadMediaItemResponse.Error;
+import com.google.photos.library.v1.util.NewMediaItemFactory;
+import com.google.photos.types.proto.MediaItem;
+import com.google.rpc.Code;
+import com.google.rpc.Status;
+
+import jp.saiki.factories.PhotosLibraryClientFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static final List<String> REQUIRED_SCOPES = ImmutableList.of(
+            "https://www.googleapis.com/auth/photoslibrary.readonly",
+            "https://www.googleapis.com/auth/photoslibrary.appendonly");
+
+
+    private static final java.io.File DATA_STORE_DIR =
+        new java.io.File(System.getProperty("user.home"), ".store/google_photos_uploader");
+
+    public static void main(final String[] args) {
+        var credential = Paths.get(System.getProperty("credential"));
+        var root = Paths.get(System.getProperty("root"));
+        var recursive = Boolean.getBoolean("recursive");
+        var depth = recursive ? Integer.MAX_VALUE : 1;
+        try {
+            HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+            try (PhotosLibraryClient client = PhotosLibraryClientFactory.createClient(credential, REQUIRED_SCOPES, httpTransport, dataStoreFactory)) {
+                List<String> uploadTokens = Files.walk(root, depth).filter( path -> Files.isRegularFile(path) ).map( f -> {
+                    try {
+                        return upload(client, f);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }).collect(Collectors.toList());
+                createMediaItem(client, uploadTokens);
+            } catch (IOException | GeneralSecurityException e) {
+                e.printStackTrace();
+            }
+        } catch(GeneralSecurityException | IOException e) {
+            e.printStackTrace();;
+        }
+    }
+
+    public static String upload(PhotosLibraryClient client, Path imagePath) throws FileNotFoundException {
+        // Create a new upload request
+        // Specify the filename that will be shown to the user in Google Photos
+        // and the path to the file that will be uploaded
+        UploadMediaItemRequest uploadRequest =
+            UploadMediaItemRequest.newBuilder()
+                //filename of the media item along with the file extension
+                .setFileName(imagePath.toFile().getName())
+                .setDataFile(new RandomAccessFile(imagePath.toFile(), "r"))
+                .build();
+        // Upload and capture the response
+        UploadMediaItemResponse uploadResponse = client.uploadMediaItem(uploadRequest);
+        if (uploadResponse.getError().isPresent()) {
+            // If the upload results in an error, handle it
+            Error error = uploadResponse.getError().get();
+            throw (ApiException) error.getCause();
+        }
+        return uploadResponse.getUploadToken().get();
+    }
+
+    public static List<MediaItem> createMediaItem(PhotosLibraryClient client, List<String> uploadTokens) {
+        List<NewMediaItem> newItems = uploadTokens.stream().map(token -> NewMediaItemFactory.createNewMediaItem(token)).collect(Collectors.toList());
+
+        List<MediaItem> result = new ArrayList<>(newItems.size());
+        BatchCreateMediaItemsResponse response = client.batchCreateMediaItems(newItems);
+        for (NewMediaItemResult itemsResponse : response.getNewMediaItemResultsList()) {
+            Status status = itemsResponse.getStatus();
+            if (status.getCode() == Code.OK_VALUE) {
+                // The item is successfully created in the user's library
+                result.add(itemsResponse.getMediaItem());
+            } else {
+            // The item could not be created. Check the status and try again
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/jp/saiki/factories/PhotosLibraryClientFactory.java
@@ -0,0 +1,60 @@
+package jp.saiki.factories;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.security.GeneralSecurityException;
+import java.util.Date;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.photos.library.v1.PhotosLibraryClient;
+import com.google.photos.library.v1.PhotosLibrarySettings;
+
+/**
+ * A factory class that helps initialize a {@link PhotosLibraryClient} instance.
+ */
+public class PhotosLibraryClientFactory {
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+  private PhotosLibraryClientFactory() {
+  }
+
+  public static PhotosLibraryClient createClient(Path clientSecretJson, List<String> selectedScopes,
+      HttpTransport httpTransport, FileDataStoreFactory dataStoreFactory) throws IOException, GeneralSecurityException {
+
+    Credential credential = authorize(clientSecretJson, selectedScopes, httpTransport, dataStoreFactory);
+    System.out.println(credential);
+    System.out.println(credential.getAccessToken());
+    System.out.println(credential.getExpirationTimeMilliseconds());
+    AccessToken accessToken = new AccessToken(credential.getAccessToken(), new Date(credential.getExpirationTimeMilliseconds()));
+    Credentials credentials = GoogleCredentials.newBuilder().setAccessToken(accessToken).build();
+    PhotosLibrarySettings settings = PhotosLibrarySettings.newBuilder()
+        .setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+    return PhotosLibraryClient.initialize(settings);
+  }
+
+  private static Credential authorize(Path credential, List<String> selectedScopes, HttpTransport httpTransport,
+      FileDataStoreFactory dataStoreFactory) throws IOException, GeneralSecurityException {
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+        new InputStreamReader(new FileInputStream(credential.toFile())));
+    // set up authorization code flow
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY,
+        clientSecrets, selectedScopes).setDataStoreFactory(dataStoreFactory).build();
+    // authorize
+    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+  }
+}
